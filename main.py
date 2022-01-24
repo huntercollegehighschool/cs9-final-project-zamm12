d36ba17575084420e30b63e5f4e2cab6aeb3227b
Jonah Dunn <94643891+zamm12@users.noreply.github.com>
2022-01-24 02:48:10 +0000

    Finished project

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 """
-Name(s):
-Name of Project:
+Name(s):Jonah Dunn
+Name of Project:Le Madlib
 """
+adjective1 = input("Enter what kind of weekend it was: ")
+noun1 = input("Enter what video game were you playing?: ")
+noun2 = input("what broke into your house?: ")
+adjective2 = input(" What did the they look like?: ")
+noun3 = input("Which room did you run too?: ")
+noun4 = input("What part of the room did you hide in?: ")
+noun5 = input("Which item did you grab?: ")
+verb1 = input("What did you do with the item towards the door?: ")
+verb2 = input("What did you do to get out of the house?: ")
+verb3 = input("What did you do since it was still chasing you?: ")
+verb4 = input(" Out of options, what did you do?: ")
 
-#Write the main part of your program here. Use of the other pages is optional.
 
-#import page1  # uncomment if you're using page1
-#import page2  # uncomment if you're using page2
-#import page3  # uncomment if you're using page3
-#import page4  # uncomment if you're using page4
+story = 'It was a ' + adjective1 + ' weekend, I sat at home playing ' + noun1 + ' when suddenly a ' + adjective2 + '  ' + noun2 + ' broke into my house, so I ran to the ' + noun3 + ' and hid in the ' + noun4 + ' I could hear its footsteps so I quickly grabbed a ' + noun5 + ' and then ' + verb1 + ' towards the door ' ' after that I ' + verb2 + ' out the window, and could still hear it chasing me so I ' + verb3 + ' but I got cornered. ' ' Out of options I ' + verb4 + ' then turned around and saw who it was ' ' ..................... ' ' it was dababy les gooo '
+
+print(story)
