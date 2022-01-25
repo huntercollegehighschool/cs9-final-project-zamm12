6c6903cebe2ad3e66a92ec92f4d45507aa422472
Jonah Dunn <94643891+zamm12@users.noreply.github.com>
2022-01-25 04:13:40 +0000

    finished project

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 Name(s):Jonah Dunn
 Name of Project:Le Madlib
 """
+import random
+
+r = random.randint(0,3)
+print(r)
+
 adjective1 = input("Enter what kind of weekend it was: ")
 noun1 = input("Enter what video game were you playing?: ")
 noun2 = input("what broke into your house?: ")
@@ -15,6 +20,57 @@ verb3 = input("What did you do since it was still chasing you?: ")
 verb4 = input(" Out of options, what did you do?: ")
 
 
-story = 'It was a ' + adjective1 + ' weekend, I sat at home playing ' + noun1 + ' when suddenly a ' + adjective2 + '  ' + noun2 + ' broke into my house, so I ran to the ' + noun3 + ' and hid in the ' + noun4 + ' I could hear its footsteps so I quickly grabbed a ' + noun5 + ' and then ' + verb1 + ' towards the door ' ' after that I ' + verb2 + ' out the window, and could still hear it chasing me so I ' + verb3 + ' but I got cornered. ' ' Out of options I ' + verb4 + ' then turned around and saw who it was ' ' ..................... ' ' it was dababy les gooo '
+story1 = 'It was a ' + adjective1 + ' weekend, I sat at home playing ' + noun1 + ' when suddenly a ' + adjective2 + '  ' + noun2 + ' broke into my house, so I ran to the ' + noun3 + ' and hid in the ' + noun4 + ' I could hear its footsteps so I quickly grabbed a ' + noun5 + ' and then ' + verb1 + ' towards the door ' ' after that I ' + verb2 + ' out the window, and could still hear it chasing me so I ' + verb3 + ' but I got cornered. ' ' Out of options I ' + verb4 + ' then turned around and saw who it was ' ' ..................... ' ' it was dababy les gooo '
 
-print(story)
+if (r == 0):
+  print(story1)
+
+descriptor1 = input("Enter what kind of smell it was: ")
+thing1 = input("Enter where the odor was coming from: ")
+thing2 = input("Describe the smell in greater detail using a noun: ")
+descriptor2 = input(" Describe the smell in greater detail using an adjective: ")
+thing3 = input("What did you grab just incase?: ")
+thing4 = input("What else did you grab: ")
+thing5 = input("Where exactly did the crashing noise come from?: ")
+action1 = input("What did you do to get away from it?: ")
+action2 = input("What did you do after to deter the noise: ")
+action3 = input("What did you do since the noise didnt stop?: ")
+action4 = input(" What did you do when it rushed at you?: ")
+thing6 =  input("What did the thing look like? ")
+
+
+story2 = 'I smelled a ' + descriptor1 + ' odor coming from ' + thing1 + ' so i went to investigate the ' + descriptor2 + '  ' + thing2 + ' that might have been causing it, just in case I grabbed a ' + thing3 + ' and a ' + thing4 + ' I heard a loud crash coming from ' + thing5 + ' so I ' + action1 + ' away from it ' ' after that I ' + action2 + ' at the noise ' + action3 + ' and heard a thud ' ' something rushed at me so i ' + action4 + ' thats when I found out it was a ' + thing6
+
+if (r == 1): 
+  print(story2)
+
+object1 = input("What did you eat?: ")
+object2 = input("What else did you eat?: ")
+descripter2 = input("How did you feel")
+object3 = input("What did you decide to get since you we'rent feeling well?: ")
+object4 = input("What else didyou decide to get?: ")
+object5 = input("Where did you get it from?: ")
+aktion1 = input("How did you get to the toilet?: ")
+object6 = input("What crashed through your wall?: ")
+aktion2 = input("How did you get out of the way?: ")
+
+
+story3 = 'I ate a ' + object1 + ' last night along with some' + object2 + ' and started feeling  ' + descripter2 + ' this morning ' ' so I decided to get some ' + object3 + ' and a ' + object4 + ' from the ' + object5 + ' after that I felt worse so I ' + aktion1 + ' to my toilet ' 'but before I could make it a ' + object6 + ' crashed throught my wall ' 'but before it hit me I' + aktion2 + ' then I went to play some among us '
+
+if (r == 2): 
+  print(story3)
+
+object1 = input("where did you drive to?: ")
+object2 = input("Where did you stop off at?: ")
+object3 = input("Describe the place: ")
+object4 = input("What did you see: ")
+object5 = input("What was it holding?: ")
+aktion1 = input("What was it doing with the object?: ")
+object6 = input("What was he building: ")
+
+
+
+story4 = 'I drove to  ' + objekt1 + ' but my car ran out of gas so i stopped off at' + objekt2 + ' it was a  ' + deskripter2 + ' place so I to stay for a little while, all of the sudden I saw a ' + objekt3 + ' off in the distance ' + objekt4 + ' and it had a ' + objekt5 + ' in its hand, which it was ' + achtion1 + ' with ' 'I could also see him building a ' + objekt6 + 'Thats when I realized it was jonsey from fortnite'
+
+if (r == 3): 
+  print(story4)
